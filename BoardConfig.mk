5a5ab3ea9beb9dca21773310706fc49651570fac
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    fog: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,7 @@ TARGET_USES_CUSTOM_DISPLAY_INTERFACE := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
