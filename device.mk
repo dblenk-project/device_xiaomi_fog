e116ae2ac8b2dc1ce28f7a45a987ce7257d394a5
boedhack99 <boedhack@gmail.com>
2023-03-01 20:32:37 +0700

    fog: Taking some perf configs to device tree
    Change-Id: I7d69832da2bbc38c6c9640d3976308acb64354c1

--- device.mk
+++ device.mk
@@ -338,6 +338,11 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
