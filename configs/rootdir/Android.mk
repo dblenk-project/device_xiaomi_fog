d92727f2577afa91c1e373c945e60415518d5510
boedhack99 <boedhack@gmail.com>
2023-04-16 16:07:09 +0700

    fog: rootdir: Re-import init.mi.usb.sh scripts * From fog 12 MIUI V13.0.5.0.SGEMIXM release-keys
    Change-Id: I3f043cc6cd718b6a9192115e810668b342b61516

--- configs/rootdir/Android.mk
+++ configs/rootdir/Android.mk
@@ -2,6 +2,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mi.usb.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.mi.usb.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.early_boot.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
