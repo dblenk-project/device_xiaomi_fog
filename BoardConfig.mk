0e576499dcf1c5a83d4fe031380deabe0c1f1d5c
boedhack99 <boedhack@gmail.com>
2023-07-26 19:03:21 +0000

    fog: Update to fog 13 MIUI V14.0.3.0.TGEMIXM release-keys

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -208,7 +208,7 @@ TARGET_RELEASETOOLS_EXTENSIONS ?= $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-05-01
+VENDOR_SECURITY_PATCH := 2023-07-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
